8e9a36414cc873dd43fa37ae88283c464aef5468
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-03-13 15:41:58 -0300

    Att

--- modulo02/sequelizerc.js
+++
@@ -1,8 +0,0 @@
-const { resolve } = require('path');
-
-module.exports = {
-    config: resolve(_dirname, 'src', 'config', 'database,js'),
-    'models-path': resolve(_dirname, 'src', 'config', 'database,js'),
-    'migrations-path': resolve(_dirname, 'src', 'config', 'database,js'),
-    'seeders-path': resolve(_dirname, 'src', 'config', 'database,js'),
-};
