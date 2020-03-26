695714ad51911a21d80a7115b6e832571326d03e
Arthur Geisweiller <50842954+geisweiller@users.noreply.github.com>
2020-03-26 12:40:05 -0300

    backend

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import Sequelize from 'sequelize'; // responsável pela conexão com  o bd
+import mongoose from 'mongoose';
 
 import User from '../app/models/User'; // importar models
 import File from '../app/models/File';
@@ -12,6 +13,7 @@ const models = [User, File, Appointment]; // array com models
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
@@ -26,6 +28,11 @@ class Database {
                 model.associate && model.associate(this.connection.models)
             ); // .map percorre o array models
     }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect('mongodb://localhost:27017/mongoapp', { useNewUrlParser: true, useFindAndModify: true });
+    }
 }
+
 
 export default new Database();
